06602d231dbe84d56cfe9b67e27e2700d698d87d
Eric Scott <scottzillaster@gmail.com>
2019-03-20 13:17:33 -0500

    removed lowercase constraint

--- utils/slackEventHelper.js
+++ utils/slackEventHelper.js
@@ -9,7 +9,7 @@ const extractLinks = (source) => {
   // source = source.toLowerCase();
   // The links/messages from slack ar always in the format <hyperlink | name >
   // this function extracts those pieces as best it can
-  source.replace(/\<http(.*?)\>/g, function(_, match){
+  source.replace(/\<http(.*?)\>/gi, function(_, match){
     const matches = ("http" + match).split("|");
     const myUrl = url.parse(matches[0]);
     const result = {
